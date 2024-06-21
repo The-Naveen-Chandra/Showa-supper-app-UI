3350d3b8054593aa2db457d92cfadb02a47a51bc
Naveen Chandra <115704630+The-Naveen-Chandra@users.noreply.github.com>
2024-06-21 23:27:54 +0530

    Dev: Constant colors, font weight & font size

---
+++ lib/constants/constant_colors.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+Color hexToColor(String hex) {
+  assert(RegExp(r'^#([0-9a-fA-F]{6})|([0-9a-fA-F]{8})$').hasMatch(hex),
+      'hex color must be #rrggbb or #rrggbbaa');
+
+  return Color(
+    int.parse(hex.substring(1), radix: 16) +
+        (hex.length == 7 ? 0xff000000 : 0x00000000),
+  );
+}
+
+class ConstantColors {
+  static Color primaryColor = hexToColor('#24459C');
+  static Color primaryTextColor = hexToColor('#4D5983');
+  static Color secondaryTextColor = hexToColor('#868DAA');
+  static Color tertiaryTextColor = hexToColor('#1E6D8B');
+  static Color whiteColor = hexToColor('#FFFFFF');
+  static Color blackColor = hexToColor('#000000');
+  static Color borderColor = hexToColor('#ECECEC');
+
+  static Color backgroundColor = hexToColor('#f4f6fa');
+
+  static Color lightGrey = hexToColor('#F3F4F6');
+  static Color grey = Colors.grey[300]!;
+  static Color darkGrey = hexToColor('#736F75');
+
+  static Color linearColor = hexToColor("#24459C");
+}
---
+++ lib/constants/constant_fontsize_fontweight.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class ConstantFontSize {
+  static const double extraExtraLarge = 30;
+  static const double extraLarge = 24;
+  static const double large = 21;
+  static const double big = 19;
+  static const double medium = 17;
+  static const double small = 15;
+  static const double extraSmall = 13;
+  static const double extraExtraSmall = 12;
+}
+
+class ConstantFontWeight {
+  static const FontWeight bold = FontWeight.w600;
+  static const FontWeight semiBold = FontWeight.w500;
+  static const FontWeight normal = FontWeight.normal;
+  static const FontWeight light = FontWeight.w300;
+}
