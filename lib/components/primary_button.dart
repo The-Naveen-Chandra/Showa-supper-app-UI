2226e7c9289de8eed88ec8eae117e28de04d345d
Naveen Chandra <115704630+The-Naveen-Chandra@users.noreply.github.com>
2024-06-21 23:30:49 +0530

    Dev: Join us screen

---
+++ lib/components/primary_button.dart
@@ -0,0 +1,88 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class PrimaryButton extends StatelessWidget {
+  final String text;
+  final Color color;
+  final Color textColor;
+  final VoidCallback onPressed;
+
+  final double verticalHeight;
+  final double fontSize;
+  final FontWeight fontWeight;
+  final Color borderColor;
+  final String iconImage;
+  final bool paddingRequired;
+
+  const PrimaryButton({
+    super.key,
+    required this.text,
+    required this.color,
+    required this.onPressed,
+    required this.verticalHeight,
+    required this.textColor,
+    required this.fontSize,
+    required this.fontWeight,
+    required this.borderColor,
+    this.iconImage = "",
+    this.paddingRequired = true,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: MediaQuery.of(context).size.width,
+      child: InkWell(
+        onTap: onPressed,
+        child: Padding(
+          padding: EdgeInsets.only(bottom: paddingRequired ? 20 : 0),
+          child: Container(
+            padding: EdgeInsets.symmetric(
+              vertical: verticalHeight,
+            ),
+            decoration: BoxDecoration(
+              color: color,
+              borderRadius: BorderRadius.circular(30),
+              border: Border.all(
+                color: borderColor,
+                width: 1,
+              ),
+            ),
+            child: iconImage.isNotEmpty
+                ? Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Image.asset(
+                        iconImage,
+                        width: 20,
+                        height: 20,
+                      ),
+                      SizedBox(
+                        width: 14,
+                      ),
+                      Text(
+                        textAlign: TextAlign.center,
+                        text,
+                        style: GoogleFonts.poppins(
+                          fontSize: fontSize,
+                          fontWeight: fontWeight,
+                          color: textColor,
+                        ),
+                      ),
+                    ],
+                  )
+                : Text(
+                    textAlign: TextAlign.center,
+                    text,
+                    style: GoogleFonts.poppins(
+                      fontSize: fontSize,
+                      fontWeight: fontWeight,
+                      color: textColor,
+                    ),
+                  ),
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/pages/join_us_screen.dart
@@ -0,0 +1,85 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:showa_supper_app/components/primary_button.dart';
+import 'package:showa_supper_app/constants/constant_colors.dart';
+import 'package:showa_supper_app/constants/constant_fontsize_fontweight.dart';
+import 'package:showa_supper_app/pages/sign_in_screen.dart';
+
+class JoinUsScreen extends StatelessWidget {
+  const JoinUsScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: ConstantColors.backgroundColor,
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Expanded(
+              flex: 10,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Center(
+                    child: Image.asset(
+                      "assets/images/showa_logo_only.png",
+                    ),
+                  ),
+                  const SizedBox(height: 20),
+                  Text(
+                    "Anything & Everything",
+                    style: GoogleFonts.poppins(
+                      fontSize: ConstantFontSize.extraLarge,
+                      fontWeight: ConstantFontWeight.bold,
+                      color: ConstantColors.primaryTextColor,
+                    ),
+                  ),
+                  const SizedBox(height: 10),
+                  Text(
+                    "Combining IoT and Modern Data\nScience for Efficient Service",
+                    textAlign: TextAlign.center,
+                    style: GoogleFonts.poppins(
+                      fontSize: ConstantFontSize.small,
+                      fontWeight: ConstantFontWeight.normal,
+                      color: ConstantColors.secondaryTextColor,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            PrimaryButton(
+              text: "Log in",
+              borderColor: ConstantColors.primaryColor,
+              color: ConstantColors.primaryColor,
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => const SignInScreen(),
+                  ),
+                );
+              },
+              verticalHeight: 12,
+              textColor: ConstantColors.whiteColor,
+              fontSize: ConstantFontSize.medium,
+              fontWeight: ConstantFontWeight.bold,
+            ),
+            PrimaryButton(
+              text: "I'm new, Sign me up",
+              color: ConstantColors.lightGrey,
+              onPressed: () {},
+              verticalHeight: 12,
+              borderColor: ConstantColors.borderColor,
+              textColor: ConstantColors.primaryColor,
+              fontSize: ConstantFontSize.medium,
+              fontWeight: ConstantFontWeight.bold,
+            ),
+            Expanded(child: Container())
+          ],
+        ),
+      ),
+    );
+  }
+}
