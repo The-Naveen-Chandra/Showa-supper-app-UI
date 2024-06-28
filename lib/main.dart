f9b99664abd61ac9ee0cdd717445cfef276a94ef
Naveen Chandra <115704630+The-Naveen-Chandra@users.noreply.github.com>
2024-06-28 21:24:08 +0530

    Refactor: Main

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-import 'package:showa_supper_app/pages/splash_screen.dart';
+import 'package:showa_supper_app/screens/splash_screen.dart';
 
 void main() {
   runApp(const MyApp());
